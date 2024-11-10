Lime3DS/Lime3DS

android: Implement Adreno Turbo setting for eligible GPUs (#227)
Co-authored-by: Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
Co-authored-by: Gamer64 <76565986+Gamer64ytb@users.noreply.github.com>
Co-authored-by: OpenSauce04 <opensauce04@gmail.com>

src/android/app/src/main/jni/native.cpp
@@ -257,6 +257,12 @@ static Core::System::ResultStatus RunCitra(const std::string& filepath) {
     return Core::System::ResultStatus::Success;
 }
 
+void EnableAdrenoTurboMode(bool enable) {
+#if defined(ENABLE_VULKAN) && CITRA_ARCH(arm64)
+    adrenotools_set_turbo(enable);
+#endif
+}
+
 void InitializeGpuDriver(const std::string& hook_lib_dir, const std::string& custom_driver_dir,
                          const std::string& custom_driver_name,
                          const std::string& file_redirect_dir) {
@@ -332,6 +338,12 @@ void JNICALL Java_io_github_lime3ds_android_NativeLibrary_initializeGpuDriver(
     jstring custom_driver_name, jstring file_redirect_dir) {
     InitializeGpuDriver(GetJString(env, hook_lib_dir), GetJString(env, custom_driver_dir),
                         GetJString(env, custom_driver_name), GetJString(env, file_redirect_dir));
+}
+
+void JNICALL Java_io_github_lime3ds_android_NativeLibrary_enableAdrenoTurboMode(JNIEnv* env,
+                                                                                jobject obj,
+                                                                                jboolean enable) {
+    EnableAdrenoTurboMode(enable);
 }
 
 void Java_io_github_lime3ds_android_NativeLibrary_notifyOrientationChange(

Raibatsu/raikopon

src/android/app/src/main/jni/native.cpp
@@ -256,6 +256,12 @@ static Core::System::ResultStatus RunCitra(const std::string& filepath) {
     return Core::System::ResultStatus::Success;
 }
 
+void EnableAdrenoTurboMode(bool enable) {
+#if defined(ENABLE_VULKAN) && CITRA_ARCH(arm64)
+    adrenotools_set_turbo(enable);
+#endif
+}
+
 void InitializeGpuDriver(const std::string& hook_lib_dir, const std::string& custom_driver_dir,
                          const std::string& custom_driver_name,
                          const std::string& file_redirect_dir) {
@@ -333,6 +339,12 @@ void JNICALL Java_org_citra_citra_1emu_NativeLibrary_initializeGpuDriver(
     jstring custom_driver_name, jstring file_redirect_dir) {
     InitializeGpuDriver(GetJString(env, hook_lib_dir), GetJString(env, custom_driver_dir),
                         GetJString(env, custom_driver_name), GetJString(env, file_redirect_dir));
+}
+
+void JNICALL Java_org_citra_citra_1emu_NativeLibrary_enableAdrenoTurboMode(JNIEnv* env,
+                                                                           jobject obj,
+                                                                           jboolean enable) {
+    EnableAdrenoTurboMode(enable);
 }
 
 void Java_org_citra_citra_1emu_NativeLibrary_notifyOrientationChange([[maybe_unused]] JNIEnv* env,