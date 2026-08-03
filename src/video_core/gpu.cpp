c88273cecf04113150e02fb470a7f02fa22913e5
Haider Zahoor <haider2090@gmail.com>
2026-08-03 07:24:39 +0500

    Fix else condition not being added in gpu.cpp
    I really hate breaking down work into separate commits.
    
    Btw, for previous commit, you will need separate changes in nxvk to make it work. Will commit those at a later time, currently experimenting.

--- src/video_core/gpu.cpp
+++ src/video_core/gpu.cpp
@@ -447,6 +447,7 @@ void GPU::SetBufferSwap(u32 screen_id, const Service::GSP::FrameBufferInfo& info
     if (screen_id == 0) {
         MicroProfileFlip();
         impl->system.perf_stats->EndGameFrame();
+    } else {
         Core::PerfStats::game_frames_updated = true;
     }
 
